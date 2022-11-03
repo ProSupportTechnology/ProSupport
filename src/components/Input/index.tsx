4790738a165dcd6a5ba484467eb18f58785e54af
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-03 19:54:11 -0300

    feat: add input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,25 @@
 import { ReactNode } from "react"
+import { RiCheckboxBlankFill } from "react-icons/ri"
+import { iInputProps } from "./types"
 
-export const Input = ({ className, label, type, register, errors }) => {
-  return <div className={className}></div>
+export const Input = ({ className, label, name, type, register, errors }: iInputProps) => {
+  return (
+    <div className={className}>
+      <label htmlFor={name} className="text three">
+        {label}
+      </label>
+      <input
+        id={name}
+        type={type}
+        {...register}
+        className={`text one ${errors?.message ? "input_red" : ""}`}
+      />
+      {/* {errors?.message && (
+        <>
+          <span className="text three">{errors?.message as ReactNode}</span>
+          <RiCheckboxBlankFill />
+        </>
+      )} */}
+    </div>
+  )
 }
-
-// criar uma forma de exibir o erro do yup como balãozinho
