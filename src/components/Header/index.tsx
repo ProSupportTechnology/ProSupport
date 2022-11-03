667f1d987a80902e1e5f8283bf5b55a9fd895253
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-03 00:13:53 -0300

    fix: creater navbar mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,85 @@
+import {
+  Barra,
+  CheckBox,
+  HeaderContainer,
+  ImgProfile,
+  InputCont,
+  LiNavBar,
+  Logo,
+  LogoutCont,
+  MenuMobile,
+  NavBar,
+  NavBarProfileContainer,
+  UlNavBar,
+} from "./style";
+import logo from "../../assets/ProSupport.png";
+import photo from "../../assets/photo.png";
+import { Link } from "react-router-dom";
+import {
+  HiHome,
+  HiUser,
+  HiChatBubbleLeftRight,
+  HiUsers,
+} from "react-icons/hi2";
+import { GiEntryDoor, GiMagnifyingGlass } from "react-icons/gi";
+
 export const Header = () => {
-  return <div>Header</div>
-}
+  return (
+    <HeaderContainer>
+      <Logo src={logo} alt="Logo" />
+      <NavBar>
+        <NavBarProfileContainer>
+          <ImgProfile src={photo} alt="imagem-profile" />
+          <h2 className="title three">Admin name</h2>
+          <span className="text three">Expert Coach</span>
+          <span className="text three">Status: Online</span>
+          <span className="text three">Email: admin@admin.com</span>
+        </NavBarProfileContainer>
+        <InputCont>
+          <input type="text" />
+          <GiMagnifyingGlass />
+        </InputCont>
+        <UlNavBar>
+          <LiNavBar>
+            <h3 className="title two">Home</h3>
+            <Link to={`/`}>
+              <HiHome />
+            </Link>
+          </LiNavBar>
+          <LiNavBar>
+            <h3 className="title two">Perfil</h3>
+            <Link to={`/`}>
+              <HiUser />
+            </Link>
+          </LiNavBar>
+          <LiNavBar>
+            <h3 className="title two">Perguntas/Respostas</h3>
+            <Link to={`/`}>
+              <HiChatBubbleLeftRight />
+            </Link>
+          </LiNavBar>
+          <LiNavBar>
+            <h3 className="title two">Usuarios</h3>
+            <Link to={`/`}>
+              <HiUsers />
+            </Link>
+          </LiNavBar>
+        </UlNavBar>
+        <LogoutCont>
+          <h3 className="title two">Sair</h3>
+          <Link to={`/`}>
+            <GiEntryDoor />
+          </Link>
+        </LogoutCont>
+      </NavBar>
+      <MenuMobile>
+        <CheckBox type="checkbox" />
+        <label htmlFor="checkbox-menu">
+          <Barra />
+          <Barra />
+          <Barra />
+        </label>
+      </MenuMobile>
+    </HeaderContainer>
+  );
+};
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,10 +1,25 @@
 import styled from "styled-components";
 
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 100px;
+  background-color: black;
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+`;
+export const Logo = styled.img`
+  width: 130px;
+  height: 100%;
+`;
 export const MenuMobile = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-right: 1rem;
+  position: relative;
+  inset: 0;
   label {
     cursor: pointer;
     position: relative;
@@ -14,8 +29,22 @@ export const MenuMobile = styled.div`
   }
 `;
 export const CheckBox = styled.input`
+  width: 100%;
+  height: 16px;
   position: absolute;
   opacity: 0;
+  z-index: 1;
+  :checked + label span:nth-child(1) {
+    transform: rotate(-45deg);
+    top: 8px;
+  }
+  :checked + label span:nth-child(2) {
+    opacity: 0;
+  }
+  :checked + label span:nth-child(3) {
+    transform: rotate(45deg);
+    top: 8px;
+  }
 `;
 export const Barra = styled.span`
   position: absolute;
@@ -25,4 +54,107 @@ export const Barra = styled.span`
   border-radius: 30px;
   background-color: grey;
   transition: 0.25s ease-in-out;
+  :nth-child(1) {
+    top: 0;
+  }
+  :nth-child(2) {
+    top: 8px;
+  }
+  :nth-child(3) {
+    top: 16px;
+  }
+`;
+export const NavBar = styled.nav`
+  position: fixed;
+  top: 100px;
+  right: 0;
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  background: #434343;
+  border-radius: 0 0 0 15px;
+`;
+export const NavBarProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 180px;
+  gap: 0.3rem;
+  background: var(--color-primary);
+  h2 {
+    color: var(--gray-0);
+  }
+  span {
+    color: var(--gray-0);
+  }
+`;
+export const ImgProfile = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+export const InputCont = styled.div`
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0.3rem;
+  border: 1px solid var(--gray-0);
+  border-radius: 5px;
+  background-color: var(--gray-3);
+  svg {
+    font-size: 2rem;
+    color: var(--gray-0);
+  }
+  input {
+    height: 100%;
+    width: 87%;
+    background-color: var(--gray-3);
+  }
+`;
+export const UlNavBar = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+`;
+export const LiNavBar = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 40px;
+  margin: 0 0.3rem;
+  a {
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 2rem;
+    color: var(--gray-0);
+  }
+  h3 {
+    color: var(--gray-0);
+  }
+`;
+export const LogoutCont = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 40px;
+  margin: 0 0.3rem;
+  margin-top: 48px;
+  a {
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 2rem;
+    color: var(--gray-0);
+  }
+  h3 {
+    color: var(--gray-0);
+  }
 `;
