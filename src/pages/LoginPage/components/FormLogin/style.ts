98e918241c47ea75acc5da2f27dd57d215b5d23c
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-04 17:46:32 -0300

    fix: add formLogin

---
+++ src/pages/LoginPage/components/FormLogin/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledFormLogin = styled.form`
+  height: max-content;
+  width: 90%;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 15px;
+  background: var(--linear-gradient-form);
+  h1 {
+    color: var(--gray-0);
+  }
+`;
