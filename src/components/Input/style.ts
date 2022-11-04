ea5b27ec953b7ee793ffbb1826336cf5f427538f
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-04 17:47:40 -0300

    feat: add icons and states

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -6,6 +6,9 @@ export const StyledInput = styled(Input)`
   position: relative;
   inset: 0;
 
+  width: 500px;
+  margin: 0 auto;
+
   label {
     position: absolute;
     left: 16px;
@@ -18,13 +21,32 @@ export const StyledInput = styled(Input)`
     padding: 10px 16px 0 16px;
     color: var(--gray-4);
     background-color: var(--gray-0);
-    border: 1px solid transparent;
     border-radius: 10px;
-
-    border: 1px solid var(--gray-2);
+    border: 1px solid transparent;
   }
 
   .input_red {
-    border: 1px solid red;
+    border: 1px solid #ed0000;
+  }
+
+  input:focus {
+    border: 1px solid var(--gray-2);
+  }
+
+  svg {
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 20px;
+    color: var(--gray-2);
+  }
+
+  .input_red + svg {
+    color: #ed0000;
+  }
+
+  .input_red:focus + svg {
+    color: var(--gray-2);
   }
 `
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,17 @@
+import { StyledInput } from "../../components/Input/style"
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>
+  return (
+    <div>
+      <br />
+      <br />
+      <StyledInput name="name" label="Nome" type="text" />
+      <br />
+      <StyledInput name="email" label="Email" type="email" />
+      <br />
+      <StyledInput name="password" label="Senha" type="password" />
+      <br />
+      <StyledInput name="confirmPassword" label="Confirmar senha" type="password" />
+    </div>
+  )
 }
