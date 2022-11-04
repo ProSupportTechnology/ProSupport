e1881ca76631b66f0eb8bf2d23faaadfc1da8cb1
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-04 16:33:58 -0300

    first commit

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,9 @@
+import { FormLogin } from "./components/FormLogin";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>
-}
+  return (
+    <div>
+      <FormLogin />
+    </div>
+  );
+};
