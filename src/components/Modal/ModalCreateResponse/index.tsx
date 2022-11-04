72c999a26fd709f1505004bc405d900e54f5adc4
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-04 20:15:48 -0300

    feat: create style ModalCreateResponse

--- src/components/Modal/ModalCreateResponse/index.tsx
+++ src/components/Modal/ModalCreateResponse/index.tsx
@@ -1,3 +1,19 @@
+import { StyledButton } from "../../../style/button/style";
+import { ModalContainer } from "../ModalContainer";
+import { StyledCreateResponse } from "./style";
+
 export const ModalCreateResponse = () => {
-  return <div>ModalCreateResponse</div>
-}
+  return (
+    <ModalContainer>
+      <StyledCreateResponse>
+        <p>Resposta</p>
+        <textarea
+          name="textResponse"
+          id="textResponse"
+          placeholder="Insira sua resposta aqui..."
+        ></textarea>
+        <StyledButton variant="default">Responder</StyledButton>
+      </StyledCreateResponse>
+    </ModalContainer>
+  );
+};
