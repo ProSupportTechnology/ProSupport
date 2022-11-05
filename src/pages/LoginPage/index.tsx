7704692aa74a5106e714b56750d7006080425991
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-05 00:12:13 -0300

    initial commit

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,9 +1,24 @@
-import { FormLogin } from "./components/FormLogin";
+import { useState } from "react"
 
-export const LoginPage = () => {
+export const SelectTechs = () => {
+  const allTechs = ["HTML", "CSS", "JavaScript", "React"]
+  const [techs, setTechs] = useState({ HTML: false, CSS: false, JavaScript: false, React: false })
+
+  function handleClick() {}
+
   return (
     <div>
-      <FormLogin />
+      <button type="button">{allTechs.join("; ")}</button>
+      <ul>
+        {allTechs.map((tech) => (
+          <li>
+            <label>
+              <input type="checkbox" name={tech} />
+              {tech}
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-};
+  )
+}
