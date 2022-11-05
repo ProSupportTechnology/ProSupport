94cdc9d5493a799e46b52bf4e626629231673298
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-05 18:51:06 -0300

    First commit

---
+++ src/pages/Profile/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyleProfile = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+export const StyleMain = styled.main`
+  margin-top: 120px;
+  width: 80%;
+  height: 100vh;
+  display: flex;
+  background-image: url
+`;
