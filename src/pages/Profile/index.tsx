e2f81d1266b0494c4e537043ad3dace4144994dd
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-05 19:34:30 -0300

    fix: style profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,39 +1,43 @@
 import { Header } from "../../components/Header";
 import { StyledImageProfile } from "../../components/ImageProfile/style";
 import photoProfile from "../../assets/photo-profile.png";
-import { StyleMain, StyleProfile } from "./style";
+import {
+  ProfileContainer,
+  ProfileDiv,
+  ProfileText,
+  StyleMain,
+  StyleProfile,
+} from "./style";
 
 export const Profile = () => {
   return (
     <StyleProfile>
       <Header />
       <StyleMain>
-        <div>
-          <div>
+        <ProfileContainer>
+          <ProfileDiv>
             <StyledImageProfile>
               <img src={photoProfile} alt="foto de perfil" />
             </StyledImageProfile>
-            <div>
+            <ProfileText>
               <h2>Admin</h2>
               <span>Expert Monitor</span>
               <span>Status: online</span>
               <span>Email: admin@prosupport.com</span>
-            </div>
-          </div>
+            </ProfileText>
+          </ProfileDiv>
           <button>Editar Perfil</button>
-        </div>
+        </ProfileContainer>
         <h1>Descrição</h1>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut l abore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut l abore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </StyleMain>
     </StyleProfile>
   );
--- src/pages/Profile/style.ts
+++ src/pages/Profile/style.ts
@@ -2,13 +2,66 @@ import styled from "styled-components";
 
 export const StyleProfile = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   display: flex;
+  background-image: url("https://i.imgur.com/xNMEX8F.png");
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 export const StyleMain = styled.main`
   margin-top: 120px;
-  width: 80%;
+  width: 100%;
   height: 100vh;
   display: flex;
-  background-image: url
+  align-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  h1 {
+    color: white;
+    width: 90%;
+    text-align: left;
+  }
+  p {
+    width: 90%;
+    color: white;
+    height: max-content;
+    text-align: center;
+    border-radius: 15px;
+    background-color: grey;
+    padding: 30px;
+  }
+`;
+export const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  button {
+    width: 120px;
+    padding: 8px 20px;
+    background-color: transparent;
+    border: solid 2px white;
+    border-radius: 15px;
+    color: white;
+  }
+`;
+export const ProfileDiv = styled.div`
+  display: flex;
+  gap: 16px;
+  figure {
+    position: static;
+    width: 100px;
+    height: 100px;
+  }
+`;
+export const ProfileText = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  h2 {
+    color: white;
+  }
+  span {
+    color: white;
+  }
 `;
