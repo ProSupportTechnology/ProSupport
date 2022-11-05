8d0696d7ce7ea490f4cd454f4ece38534464d7c2
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-05 00:24:42 -0300

    feat: add SelectTechs base

--- src/components/SelectTechs/index.tsx
+++ src/components/SelectTechs/index.tsx
@@ -1,3 +1,24 @@
+import { useState } from "react"
+
 export const SelectTechs = () => {
-  return <div>SelectTechs</div>
+  const allTechs = ["HTML", "CSS", "JavaScript", "React"]
+  const [techs, setTechs] = useState({ HTML: false, CSS: false, JavaScript: false, React: false })
+
+  function handleClick() {}
+  // setTechs({ ...techs, tech: event.target.value })
+  return (
+    <div>
+      <button type="button">{allTechs.join("; ")}</button>
+      <ul>
+        {allTechs.map((tech) => (
+          <li>
+            <label>
+              <input onChange={(event) => console.log(event.target)} type="checkbox" name={tech} />
+              {tech}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,24 +1,3 @@
-import { useState } from "react"
-
-export const SelectTechs = () => {
-  const allTechs = ["HTML", "CSS", "JavaScript", "React"]
-  const [techs, setTechs] = useState({ HTML: false, CSS: false, JavaScript: false, React: false })
-
-  function handleClick() {}
-
-  return (
-    <div>
-      <button type="button">{allTechs.join("; ")}</button>
-      <ul>
-        {allTechs.map((tech) => (
-          <li>
-            <label>
-              <input type="checkbox" name={tech} />
-              {tech}
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+export const LoginPage = () => {
+  return <div></div>
 }
