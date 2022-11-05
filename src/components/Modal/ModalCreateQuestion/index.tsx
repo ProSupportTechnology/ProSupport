b77740f53a45af217294f7fdd8919a01ccfa87d0
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-04 21:08:19 -0300

    feat: add style ModalCreateQuestion

--- src/components/Modal/ModalCreateQuestion/index.tsx
+++ src/components/Modal/ModalCreateQuestion/index.tsx
@@ -1,3 +1,23 @@
+import { StyledButton } from "../../../style/button/style";
+import { Input } from "../../Input";
+import { ModalContainer } from "../ModalContainer";
+import { StyledCreateQuestion } from "./style";
+
 export const ModalCreateQuestion = () => {
-  return <div>ModalCreateQuestion</div>
-}
+  return (
+    <ModalContainer>
+      <StyledCreateQuestion>
+        <p>Insira sua questão</p>
+        <Input type="text" label="Título" name="title" />
+        <Input type="text" label="Tecnologia" name="technology" />
+        <label htmlFor="textQuestion">Descrição</label>
+        <textarea
+          name="textQuestion"
+          id="textQuestion"
+          placeholder="Insira sua questão aqui..."
+        ></textarea>
+        <StyledButton variant="default">Responder</StyledButton>
+      </StyledCreateQuestion>
+    </ModalContainer>
+  );
+};
