6ca18cdfa55d497b76acf852e539c28be830ba91
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-04 21:15:18 -0300

    feat: create style ModalEditQuestion

--- src/components/Modal/ModalEditQuestion/index.tsx
+++ src/components/Modal/ModalEditQuestion/index.tsx
@@ -1,3 +1,23 @@
+import { StyledButton } from "../../../style/button/style";
+import { Input } from "../../Input";
+import { ModalContainer } from "../ModalContainer";
+import { StyledEditQuestion } from "./style";
+
 export const ModalEditQuestion = () => {
-  return <div>ModalEditQuestion</div>
-}
+  return (
+    <ModalContainer>
+      <StyledEditQuestion>
+        <p>Edite sua questão</p>
+        <Input type="text" label="Título" name="title" />
+        <Input type="text" label="Tecnologia" name="technology" />
+        <label htmlFor="textQuestion">Descrição</label>
+        <textarea
+          name="textQuestion"
+          id="textQuestion"
+          placeholder="Edite sua questão aqui..."
+        ></textarea>
+        <StyledButton variant="default">Responder</StyledButton>
+      </StyledEditQuestion>
+    </ModalContainer>
+  );
+};
---
+++ src/components/Modal/ModalEditQuestion/style.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const StyledEditQuestion = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 340px;
+  width: 290px;
+  color: var(--gray-0);
+
+  p {
+    font-size: 20px;
+    margin: 15px 0;
+  }
+
+  label {
+    font-size: 12px;
+    display: flex;
+    margin-left: 10px;
+  }
+
+  input {
+    font-size: 10px;
+    padding: 0 10px;
+    margin-bottom: 10px;
+    width: 280px;
+    height: 35px;
+    border-radius: 5px;
+    color: var(--gray-0);
+    border: 1px solid var(--gray-1);
+    background-color: var(--background);
+  }
+
+  textarea {
+    padding: 10px;
+    color: var(--gray-0);
+    resize: none;
+    align-self: center;
+    width: 280px;
+    height: 140px;
+    border-radius: 10px;
+    border: 1px solid var(--gray-1);
+    background-color: var(--background);
+  }
+
+  button {
+    width: 97px;
+    height: 35px;
+    align-self: flex-end;
+    margin: 5px 10px;
+  }
+
+  @media (min-width: 768px) {
+    height: 450px;
+    width: 530px;
+    font-size: 25px;
+
+    p {
+      margin: 25px 0;
+    }
+
+    textarea {
+      font-size: 20px;
+      width: 100%;
+    }
+
+    label {
+      margin-left: 0;
+    }
+
+    input {
+      width: 100%;
+      height: 45px;
+    }
+
+    button {
+      width: 105px;
+      height: 40px;
+      margin: 15px 0;
+    }
+  }
+`;
