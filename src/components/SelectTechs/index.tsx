8c4eab53529090b19fef54d9885c53d9216d5416
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-05 20:47:55 -0300

    feat: add state required

--- src/components/SelectTechs/index.tsx
+++ src/components/SelectTechs/index.tsx
@@ -1,11 +1,17 @@
-import { ChangeEvent, useState, useEffect } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { ContainerSelectTechs } from "./style"
 import { RiCheckboxBlankFill } from "react-icons/ri"
+import { iStyledSelectTechs } from "./types"
 
-export const StyledSelectTechs = () => {
+export const StyledSelectTechs = ({ techs, setTechs }: iStyledSelectTechs) => {
   const allTechs = ["HTML", "CSS", "JavaScript", "React", "TypeScript"]
-  const [techs, setTechs] = useState(allTechs.map((tech) => ({ name: tech, checked: false })))
+  // const [techs, setTechs] = useState(allTechs.map((tech) => ({ name: tech, checked: false })))
   const [isOpened, setIsOpened] = useState(false)
+
+  useEffect(() => {
+    setTechs(allTechs.map((tech) => ({ name: tech, checked: false })))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   function handleEvent(event: ChangeEvent<HTMLInputElement>, actualTech: string) {
     setTechs(
@@ -14,14 +20,16 @@ export const StyledSelectTechs = () => {
   }
 
   function selectInnerText() {
-    const actualTech = techs.map((tech) => tech.checked == true && tech.name).filter((tech) => tech !== false)
-    return actualTech.join("; ")
+    const actualTech = techs
+      .map((tech) => tech.checked === true && tech.name)
+      .filter((tech) => tech !== false) as string[]
+    return actualTech
   }
 
   return (
     <ContainerSelectTechs liCount={allTechs.length}>
       <button onClick={() => setIsOpened(!isOpened)} type="button">
-        {selectInnerText() || "Selecione a tecnologia"}
+        {selectInnerText().join("; ") || "Selecione a tecnologia"}
       </button>
       <ul className={isOpened ? "is_opened" : ""}>
         <RiCheckboxBlankFill />
