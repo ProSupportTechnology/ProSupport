645544da31ddedc321d611c29a3a5760fdc3ed39
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-06 15:51:13 -0300

    feat: create validations from ModalEditResponse

--- src/components/Modal/ModalEditResponse/index.tsx
+++ src/components/Modal/ModalEditResponse/index.tsx
@@ -3,16 +3,36 @@ import { StyledTextArea } from "../../TextArea/style";
 import { StyledButton } from "../../../style/button/style";
 import { StyledModalResponse } from "../../../style/modalResponse/style";
 import { ModalContainer } from "../ModalContainer";
+import { useForm } from "react-hook-form";
+import { iDataResponse } from "../../../contexts/QuestionContext/types";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ResponseSchema } from "../../../schemas/responseSchemas";
 
 export const ModalEditResponse = () => {
   const { setIsModEditRespOpen } = useQuestionContext();
+
+  const { register, handleSubmit } = useForm<iDataResponse>({
+    resolver: yupResolver(ResponseSchema),
+  });
+
+  function editResponse(data: iDataResponse) {
+    console.log(data);
+    //RECEBE UM ID DA RESPOSTA
+    //realizar requisição da pergunta com o data como body
+    //PATCH/responses/IdResposta
+    //Toast correpondentes no try/catch
+    //setIsModEditRespOpen(false) em caso de sucesso
+  }
+
+  //pensar numa lógica para renderizar o conteúdo da resposta no textarea
 
   return (
     <ModalContainer setIsModOpen={setIsModEditRespOpen}>
       <StyledModalResponse>
         <p>Editar resposta</p>
-        <form>
+        <form onSubmit={handleSubmit(editResponse)}>
           <StyledTextArea
+            register={register("description")}
             placeholder="Edite sua resposta aqui"
             label="Edite sua resposta"
           />
