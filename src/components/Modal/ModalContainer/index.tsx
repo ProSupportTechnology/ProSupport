9645b10b64e5e50980c5d6fb631e655f591ec922
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-06 12:11:02 -0300

    feat: create logic to close modal

--- src/components/Modal/ModalContainer/index.tsx
+++ src/components/Modal/ModalContainer/index.tsx
@@ -1,11 +1,30 @@
+import { MouseEvent } from "react";
+import { CgClose } from "react-icons/cg";
 import { Modal } from "./style";
 import { iContainerModalProps } from "./types";
 
-export const ModalContainer = ({ children }: iContainerModalProps) => {
+export const ModalContainer = ({
+  children,
+  setIsModOpen,
+}: iContainerModalProps) => {
+  function closeModal() {
+    setIsModOpen((oldIsMod) => !oldIsMod);
+  }
+
+  function handleClick(e: MouseEvent) {
+    let target = e.target as HTMLElement;
+    target.className === "overlay" && closeModal();
+  }
+
   return (
     <Modal>
-      <div className="overlay">
-        <div className="content">{children}</div>
+      <div className="overlay" onClick={(e) => handleClick(e)}>
+        <div className="content">
+          <span onClick={closeModal}>
+            <CgClose />
+          </span>
+          {children}
+        </div>
       </div>
     </Modal>
   );
