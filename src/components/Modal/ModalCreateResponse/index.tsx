bb7c589cd99b75a5110777623d142f7aa27e17ba
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-06 15:43:13 -0300

    feat: create validations to ModalCreateResponse

--- src/components/Modal/ModalCreateResponse/index.tsx
+++ src/components/Modal/ModalCreateResponse/index.tsx
@@ -1,4 +1,8 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import { useQuestionContext } from "../../../contexts/QuestionContext";
+import { iDataResponse } from "../../../contexts/QuestionContext/types";
+import { ResponseSchema } from "../../../schemas/responseSchemas";
 import { StyledButton } from "../../../style/button/style";
 import { StyledModalResponse } from "../../../style/modalResponse/style";
 import { StyledTextArea } from "../../TextArea/style";
@@ -7,12 +11,31 @@ import { ModalContainer } from "../ModalContainer";
 export const ModalCreateResponse = () => {
   const { setIsModCreateRespOpen } = useQuestionContext();
 
+  const { register, handleSubmit } = useForm<iDataResponse>({
+    resolver: yupResolver(ResponseSchema),
+  });
+
+  function sendResponse(data: iDataResponse) {
+    //recebe userId
+    //recebe questionId
+    const body = { ...data, questionId: 1, userId: 1 };
+    console.log(body);
+    //realizar requisição da pergunta com o body acima
+    //POST/responses
+    //Toast correpondentes no try/catch
+    //setIsModCreateRespOpen(false) em caso de sucesso
+  }
+
   return (
     <ModalContainer setIsModOpen={setIsModCreateRespOpen}>
       <StyledModalResponse>
         <p>Resposta</p>
-        <form>
-          <StyledTextArea placeholder="Insira sua resposta aqui" />
+        <form onSubmit={handleSubmit(sendResponse)}>
+          <StyledTextArea
+            register={register("description")}
+            label="Resposta"
+            placeholder="Insira sua resposta aqui"
+          />
           <StyledButton variant="default">Responder</StyledButton>
         </form>
       </StyledModalResponse>
