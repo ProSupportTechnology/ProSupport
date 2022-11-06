0b81b57f0682d73c8251377090e64e94d2040351
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-06 10:30:48 -0300

    feat: add logic to open modalEditQuestion

--- src/components/Modal/ModalEditQuestion/index.tsx
+++ src/components/Modal/ModalEditQuestion/index.tsx
@@ -1,25 +1,44 @@
+import { CgClose } from "react-icons/cg";
+import { useQuestionContext } from "../../../contexts/QuestionContext";
 import { StyledButton } from "../../../style/button/style";
 import { ModalQuestion } from "../../../style/modalQuestion/style";
-import { Input } from "../../Input";
+import { StyledInput } from "../../Input/style";
+import { StyledTextArea } from "../../TextArea/style";
 import { ModalContainer } from "../ModalContainer";
 
 export const ModalEditQuestion = () => {
+  const { isModEditQuestOpen, setIsModEditQuestOpen } = useQuestionContext();
+
   return (
     <ModalContainer>
-      <form>
-        <ModalQuestion>
-          <p>Edite sua questão</p>
-          <Input type="text" label="Título" name="title" />
-          <Input type="text" label="Tecnologia" name="technology" />
-          <label htmlFor="textQuestion">Descrição</label>
-          <textarea
-            name="textQuestion"
-            id="textQuestion"
-            placeholder="Edite sua questão aqui..."
-          ></textarea>
+      <ModalQuestion>
+        <span
+          onClick={() => {
+            setIsModEditQuestOpen(!isModEditQuestOpen);
+          }}
+        >
+          <CgClose />
+        </span>
+        <p>Edite sua questão</p>
+        <form>
+          <StyledInput
+            label="Título"
+            name="title"
+            type="text"
+            className="isModal"
+            modalPlaceholder="Edite o título"
+          />
+          <StyledInput
+            label="Tecnologia"
+            name="techs"
+            type="text"
+            className="isModal"
+            modalPlaceholder="Edite a tecnologia"
+          />
+          <StyledTextArea label="Descrição" placeholder="Edite sua Descrição" />
           <StyledButton variant="default">Responder</StyledButton>
-        </ModalQuestion>
-      </form>
+        </form>
+      </ModalQuestion>
     </ModalContainer>
   );
 };
--- src/contexts/QuestionContext/index.tsx
+++ src/contexts/QuestionContext/index.tsx
@@ -7,16 +7,24 @@ interface iQuestionContextProps {
 export interface iQuestionContext {
   isModCreateQuestOpen: boolean;
   setIsModCreateQuestOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isModEditQuestOpen: boolean;
+  setIsModEditQuestOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const QuestionContext = createContext<iQuestionContext>({} as iQuestionContext);
 
 export const QuestionProvider = ({ children }: iQuestionContextProps) => {
   const [isModCreateQuestOpen, setIsModCreateQuestOpen] = useState(false);
+  const [isModEditQuestOpen, setIsModEditQuestOpen] = useState(false);
 
   return (
     <QuestionContext.Provider
-      value={{ isModCreateQuestOpen, setIsModCreateQuestOpen }}
+      value={{
+        isModCreateQuestOpen,
+        setIsModCreateQuestOpen,
+        isModEditQuestOpen,
+        setIsModEditQuestOpen,
+      }}
     >
       {children}
     </QuestionContext.Provider>
