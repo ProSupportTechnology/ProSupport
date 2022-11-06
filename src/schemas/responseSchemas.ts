25a379186fddb0ec18d092122ddde5ea3e5072d1
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-06 15:41:53 -0300

    fix: adjust responseSchema

--- src/schemas/responseSchemas.ts
+++ src/schemas/responseSchemas.ts
@@ -2,10 +2,4 @@ import * as yup from "yup";
 
 export const ResponseSchema = yup.object({
   description: yup.string().required("Descrição é obrigatória"),
-  questionId: yup.number().required("Id é obrigatório"),
-  userId: yup.number().required("Id é obrigatório"),
-});
-
-export const EditResponseSchema = yup.object({
-  description: yup.string().required("Descrição é obrigatória"),
 });
