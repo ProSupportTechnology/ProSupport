39a658f2cbdb5fe44ebc534c0fa54164301add28
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-07 16:01:49 -0300

    fix: clear code

--- src/contexts/UserContext/types.ts
+++ src/contexts/UserContext/types.ts
@@ -1,5 +1,3 @@
-import { BlobOptions } from "buffer";
-
 export interface iRegister {
   name: string;
   email: string;
@@ -37,8 +35,8 @@ export interface iUserContext {
   handleRegister(data: iRegister): Promise<void>;
   handleLogin(data: iLogin): Promise<void>;
   user: iUser;
-  loading: boolean
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export interface iQuestion {
