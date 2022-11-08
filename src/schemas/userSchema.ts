d7cf1b7d1e400a8b87c0b64c595e11c8d3e9bc88
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-08 13:50:44 -0300

    fix: adjust userSchema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,13 +1,15 @@
 import * as yup from "yup";
 
 export const userSchema = yup.object({
-  title: yup.string(),
-  email: yup.string().email("email inválido"),
-  password: yup.string(),
-  // .matches(/[a-z]/, "Deve haver ao menos uma letra minúscula")
-  // .matches(/(\d)/, "Deve haver ao menos um número")
-  // .matches(/(\W)|_/, "Deve haver ao menos um caracter especial")
-  // .matches(/.{8,}/, "Deve haver no mínimo 8 caracteres"),
-  bio: yup.string(),
-  photo: yup.string(),
+  name: yup.string().required(),
+  email: yup.string().email("email inválido").required(),
+  password: yup
+    .string()
+    .matches(/[a-z]/, "Deve haver ao menos uma letra minúscula")
+    .matches(/(\d)/, "Deve haver ao menos um número")
+    .matches(/(\W)|_/, "Deve haver ao menos um caracter especial")
+    .matches(/.{8,}/, "Deve haver no mínimo 8 caracteres")
+    .required(),
+  bio: yup.string().required(),
+  image: yup.string().required(),
 });
