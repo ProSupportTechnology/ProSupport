a8603445a9c70e28f5f7bd863bb660d043bd3b80
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-08 19:07:13 -0300

    fix: card question reform style

--- src/components/QuestionCard/style.ts
+++ src/components/QuestionCard/style.ts
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 
-export const StyledQuestionCard = styled.div`
+export const StyledQuestionCard = styled.li`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+  align-items: center;
+  height: 200px;
+  width: 98%;
   color: var(--gray-0);
-  gap: 0.5rem;
+  gap: 8px;
   transition: 0.5s;
-  padding: 0px;
+  background: -webkit-linear-gradient(to left, #000000, #434343);
+  background: linear-gradient(to left, #000000, #434343);
+  border: 2px solid var(--gray-1);
+  border-radius: 20px;
 
-  &:hover > div {
+  &:hover {
     background: #4f4d8c;
     background: linear-gradient(90deg, #4f4d8c 0%, #807ddb 55%, #5f5da6 100%);
     background: -webkit-linear-gradient(
@@ -21,130 +25,42 @@ export const StyledQuestionCard = styled.div`
     );
     box-shadow: 10px 10px 20px black;
   }
-
-  span {
-    padding: 0 20px;
-    width: 100%;
-    text-align: left;
-    font-size: 0.9rem;
+  .imageContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    width: 145px;
   }
 
-  div {
+  .textContainer {
+    height: 100%;
+    width: 60%;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    gap: 16px;
+  }
+  .btnContainer {
+    width: 20%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    gap: 1rem;
-    padding: 60px 35px;
-    background: #000000;
-    background: -webkit-linear-gradient(to left, #000000, #434343);
-    background: linear-gradient(to left, #000000, #434343);
-    border: 1px solid var(--gray-1);
-    border-radius: 20px;
-    transition: 0.5s;
-    width: 97%;
-    padding: 10px;
-
-    .imageContainer {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-      padding: 0;
-      min-width: 50px;
-      min-height: 50px;
-      background: none;
-      border: none;
-      border-radius: 50%;
-      cursor: pointer;
-      width: 20%;
-
-      figure {
-        border-radius: 50%;
-        width: 90px;
-        height: 90px;
-        background: #000000;
-        background: -webkit-linear-gradient(to left, #000000, #434343);
-        background: linear-gradient(to left, #000000, #434343);
-        box-shadow: 5px 10px 10px var(--gray-3);
-      }
-
-      img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        object-fit: cover;
-      }
-    }
-
-    .textContainer {
-      /* position: relative; */
-      display: flex;
-      flex-direction: row;
-      background: none;
-      border: none;
-      border-radius: none;
-      width: 100%;
-
-      padding: 0px;
-      background: none;
-      border: none;
-      align-items: center;
-      width: 100%;
-      height: 100%;
-
-      h2 {
-        font-size: 1rem;
-        height: auto;
-      }
-
-      .questionContent {
-        border: none;
-        overflow: auto;
-        min-height: 100%;
-        padding: 5px;
-        text-align: center;
-        background: none;
-        overflow-y: auto;
-      }
-    }
-
-    .techContainer {
-      width: 100%;
-      height: 30px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: flex-end;
-      align-self: end;
-      border: none;
-      background: none;
-
-      div {
-        border: 1px solid var(--gray-1);
-        border-radius: 5px;
-        height: 25px;
-        width: 50%;
-        background: none;
-        display: flex;
-        align-items: center;
-        padding: 4px;
-        min-width: 85px;
-        max-width: 100px;
-      }
-
-      h2 {
-        font-size: 0.8rem;
-        padding: 0px;
-      }
-    }
-
-    .button {
-      transform: scale(2);
-
+  }
+  .techConatiner {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    button {
+      background-color: transparent;
+      color: var(--white);
+      font-size: 1rem;
       &:hover {
-        transform: scale(2.5);
-        transition: 0.5s;
-      }
+      transform: scale(1.2);
+      transition: 0.1s;
+    }
     }
   }
 
@@ -153,8 +69,7 @@ export const StyledQuestionCard = styled.div`
   }
 
   @media (min-width: 768px) {
-    padding: 10px;
-    height: 250px;
+    height: 200px;
 
     .imageContainer {
       border-radius: 20px;
