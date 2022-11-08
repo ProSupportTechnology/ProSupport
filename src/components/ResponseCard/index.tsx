e15839228e3acbedd8ef3869c602d36d862bf26c
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-08 13:54:43 -0300

    fix: update buttons ResponseCard

--- src/components/ResponseCard/index.tsx
+++ src/components/ResponseCard/index.tsx
@@ -3,6 +3,8 @@ import { StyledResponseCard } from "./style";
 import { TiPencil } from "react-icons/ti";
 import { iResponseCard } from "./types";
 import userImg from "../../assets/photo.png";
+import { CgClose } from "react-icons/cg";
+import { useQuestionContext } from "../../contexts/QuestionContext";
 
 export const ResponseCard = ({
   username,
@@ -10,29 +12,44 @@ export const ResponseCard = ({
   array,
   children,
 }: iResponseCard) => {
+  const { setResponseId, setIsModEditRespOpen, setIsModDeleteRespOpen } =
+    useQuestionContext();
   return (
     <>
       {array.map((element: any) => {
         return (
           <StyledResponseCard>
-         <div>
-        <div className="techContainer">
-          <TiPencil className="buttonClose"></TiPencil>
-        </div>
-        <div className="textContainer">
-          <div className="imageContainer">
-            <StyledImageProfile>
-              <img src={image ? image : userImg} alt="userphoto" />
-            </StyledImageProfile>
-            <h2>{username}</h2>
-          </div>
-          <div className="questionContent">
-            <h2>Resposta:</h2>
-            <p>{element.description}</p>
-            {children}
-          </div>
-        </div>
-      </div>
+            <div>
+              <div className="techContainer">
+                <TiPencil
+                  className="buttonClose"
+                  onClick={() => {
+                    setIsModEditRespOpen(true);
+                    setResponseId(element.id);
+                  }}
+                />
+                <CgClose
+                  className="buttonClose"
+                  onClick={() => {
+                    setResponseId(element.id);
+                    setIsModDeleteRespOpen(true);
+                  }}
+                />
+              </div>
+              <div className="textContainer">
+                <div className="imageContainer">
+                  <StyledImageProfile>
+                    <img src={image ? image : userImg} alt="userphoto" />
+                  </StyledImageProfile>
+                  <h2>{username}</h2>
+                </div>
+                <div className="questionContent">
+                  <h2>Resposta:</h2>
+                  <p>{element.description}</p>
+                  {children}
+                </div>
+              </div>
+            </div>
           </StyledResponseCard>
         );
       })}
