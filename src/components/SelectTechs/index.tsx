c905b8a7805743aa4cdc7d15a976b8abbd719ac7
Thomas Schreiner <iteano.thom@hotmail.com>
2022-11-09 01:18:24 -0300

    feat: add defaults value of select

--- src/components/SelectTechs/index.tsx
+++ src/components/SelectTechs/index.tsx
@@ -4,7 +4,7 @@ export const SelectTechs = ({ className, register, id }: iStyledSelectTechs) => 
   const allTechs = ["HTML", "CSS", "JavaScript", "React", "TypeScript"];
 
   return (
-    <select className={`text two ${className}`} {...register} id={id}>
+    <select className={`text two ${className}`} {...register} id={id} defaultValue={allTechs[0]}>
       {allTechs.map((tech) => (
         <option key={tech} value={tech === "JavaScript" ? "JS" : tech === "TypeScript" ? "TS" : tech}>
           {tech}
