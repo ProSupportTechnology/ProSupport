dc7483d8b69026a37aeb344a943c365c4b2dd58d
Henrique Jesus <henrique.jesus128@gmail.com>
2022-11-10 00:44:06 -0300

    fix: brackground color card response

--- src/components/ResponseCard/style.ts
+++ src/components/ResponseCard/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { StyledQuestionCard } from "../QuestionCard/style";
 
 export const StyledResponseCard = styled(StyledQuestionCard)`
-  background: var(--gray-4);
+  background: var(--gray-1);
   margin: 1rem 0;
   .techConatiner {
     justify-content: flex-end;
